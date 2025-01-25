24883a52e03802061a5831e923a28a36506a8901
nao-nba <ba.d.yoa.b.tw@gmail.com>
2025-01-26 02:16:30 +0900

    ログイン機能（css,js以外）の実装

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.Login.as_view(), name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import User, Timer, Category, Study_log, Goal
+from .forms import LoginForm
+from django.contrib.auth.views import LoginView
+from django.views import generic
+
 
 def index(request):
     categories = Category.objects.all() 
     return render(request, 'app/index.html' , {'categories': categories})
+
+class Login(LoginView):
+    form_class = LoginForm
+    template_name = 'app/login.html'
