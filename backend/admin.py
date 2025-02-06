6bfa78219e3847136b64aad93ec664e1b21ed468
sasakifuruta <rixiaozig@gmail.com>
2025-02-07 00:29:01 +0900

    ディレクトリ修正

--- backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Timer, Category, Study_log, Goal
-
-admin.site.register(User)
-admin.site.register(Timer)
-admin.site.register(Category)
-admin.site.register(Study_log)
-admin.site.register(Goal)
