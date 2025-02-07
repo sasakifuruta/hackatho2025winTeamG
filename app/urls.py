83e8a99d7957e2fe4a2a525456309ca68f4bd6c6
nao-nba <ba.d.yoa.b.tw@gmail.com>
2025-02-07 21:41:19 +0900

    認証のバックエンド処理が完成

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('signup/', views.Signup.as_view(), name='signup'),
-]
+    path('account/', views.update_profile, name='account'),
+]   
