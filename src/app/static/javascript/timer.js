2f49b4b5c451cde388e38764c4132673fbac75f4
= <=>
2025-02-16 06:18:28 +0900

    Revert "[fix] conect models, [feat]rest time,[feat]add category, etc"
    This reverts commit aaae36be8d38f443548dd433fb723826909ebed9.

--- src/app/static/javascript/timer.js
+++ src/app/static/javascript/timer.js
@@ -5,40 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const resumeButton = document.getElementById("resume-timer");
   const resetButton = document.getElementById("reset-timer");
   const modeSelect = document.getElementById("mode-select");
-  const categorySelect = document.getElementById("category-select");
-  const stopRestButton = document.getElementById("stop-rest-timer");
-  const statusText = document.getElementById("status-text");
 
   let interval; // タイマーのID
   let targetTime; // カウントダウンの終了時間
   let startTime; // カウントダウンの開始時間
   let remainingTime = 0; // 一時停止時の残り時間
   let isPaused = false; // 一時停止状態かどうか
-  let isStudy = true; // 勉強時間かどうか（falseの場合は休憩時間）
 
   // 残り時間を表示する関数
   function updateCountDown() {
     const now = new Date().getTime(); // 現在の時刻を取得（ミリ秒単位）
     const distance = targetTime - now;
-    if (isStudy) {
-      statusText.textContent = "作業中";
-    } else {
-      statusText.textContent = "休憩中";
-    }
 
     if (distance <= 0) {
       clearInterval(interval); // タイマーを停止
-      if (isStudy) {
-        storeElapsedTime();
-        startRestTimer();
-      } else {
-        startButton.disabled = false;
-        modeSelect.disabled = false;
-        categorySelect.disabled = false;
-        stopRestButton.disabled = true;
-        isStudy = true;
-        statusText.textContent = "準備中";
-      }
+      countDown.textContent = "終了しました";
+      storeElapsedTime();
+      modeSelect.disabled = false;
       return;
     }
 
@@ -59,62 +42,19 @@ document.addEventListener("DOMContentLoaded", function () {
       clearInterval(interval);
     }
 
-    fetch("/home/api/get_study_time")
-      .then((response) => response.json())
-      .then((data) => {
-        let workTime = data.study || 25; // 設定された作業時間を取得（デフォルトは60分）
-        workTime = parseInt(workTime, 10); // 数値に変換
+    let workTime = localStorage.getItem("workTime") || 60; // 設定された作業時間を取得（デフォルトは60分）
+    workTime = parseInt(workTime, 10); // 数値に変換
 
-        startTime = new Date().getTime(); // 作業開始時間を記録
-        targetTime = new Date().getTime() + workTime * 60000; // 選択された時間でカウントダウン開始
-        isStudy = true;
-        isPaused = false;
+    startTime = new Date().getTime(); // 作業開始時間を記録
+    targetTime = new Date().getTime() + workTime * 60000; // 選択された時間でカウントダウン開始
+    interval = setInterval(updateCountDown, 1000); // 1000ミリ秒（1秒）ごとにupdateCountDownを行う
+    updateCountDown();
 
-        interval = setInterval(updateCountDown, 1000); // 1000ミリ秒（1秒）ごとにupdateCountDownを行う
-        updateCountDown();
-
-        pauseButton.disabled = false;
-        resumeButton.disabled = true;
-        resetButton.disabled = false;
-        modeSelect.disabled = true;
-        categorySelect.disabled = true;
-      })
-      .catch((error) => {
-        console.error("作業時間の取得に失敗！", error);
-      });
-  }
-
-  // 休憩タイマーを起動する関数
-  function startRestTimer() {
-    if (interval) {
-      clearInterval(interval);
-    }
-
-    fetch("/home/api/get_rest_time")
-      .then((response) => response.json())
-      .then((data) => {
-        let restTime = data.rest || 5;
-        restTime = parseInt(restTime, 10);
-
-        startTime = new Date().getTime(); // 作業開始時間を記録
-        targetTime = new Date().getTime() + restTime * 60000; // 選択された時間でカウントダウン開始
-        isStudy = false;
-        isPaused = false;
-
-        interval = setInterval(updateCountDown, 1000); // 1000ミリ秒（1秒）ごとにupdateCountDownを行う
-        updateCountDown();
-
-        startButton.disabled = true;
-        pauseButton.disabled = true;
-        resumeButton.disabled = true;
-        resetButton.disabled = true;
-        modeSelect.disabled = true;
-        categorySelect.disabled = true;
-        stopRestButton.disabled = false;
-      })
-      .catch((error) => {
-        console.error("休憩時間の取得に失敗！", error);
-      });
+    isPaused = false;
+    pauseButton.disabled = false;
+    resumeButton.disabled = true;
+    resetButton.disabled = false;
+    modeSelect.disabled = true;
   }
 
   // タイマーを一時停止する関数
@@ -146,59 +86,21 @@ document.addEventListener("DOMContentLoaded", function () {
     clearInterval(interval);
     storeElapsedTime();
 
-    fetch("/home/api/get_study_time/")
-      .then((response) => response.json())
-      .then((data) => {
-        let workTime = data.study || 25;
-        workTime = parseInt(workTime, 10);
-        let hours = Math.floor(workTime / 60);
-        let minutes = workTime % 60;
-        countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
-          minutes
-        ).padStart(2, "0")}:00`;
+    let workTime = localStorage.getItem("workTime") || 60;
+    workTime = parseInt(workTime, 10);
+    let hours = Math.floor(workTime / 60);
+    let minutes = workTime % 60;
+    countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
+      minutes
+    ).padStart(2, "0")}:00`;
 
-        remainingTime = 0;
-        isPaused = false;
+    remainingTime = 0;
+    isPaused = false;
 
-        pauseButton.disabled = true;
-        resumeButton.disabled = true;
-        resetButton.disabled = true;
-        modeSelect.disabled = false;
-        categorySelect.disabled = false;
-      })
-      .catch((error) => {
-        console.error("ダメでした！", error);
-      });
-  }
-
-  // 休憩タイマーをストップする関数
-  function stopRestTimer() {
-    clearInterval(interval);
-
-    fetch("/home/api/get_study_time/")
-      .then((response) => response.json())
-      .then((data) => {
-        let workTime = data.study || 25;
-        workTime = parseInt(workTime, 10);
-        let hours = Math.floor(workTime / 60);
-        let minutes = workTime % 60;
-        countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
-          minutes
-        ).padStart(2, "0")}:00`;
-
-        remainingTime = 0;
-        isPaused = false;
-
-        startButton.disabled = false;
-        modeSelect.disabled = false;
-        categorySelect.disabled = false;
-        stopRestButton.disabled = true;
-        isStudy = true;
-        statusText.textContent = "作業中";
-      })
-      .catch((error) => {
-        console.error("ダメでした！", error);
-      });
+    pauseButton.disabled = true;
+    resumeButton.disabled = true;
+    resetButton.disabled = true;
+    modeSelect.disabled = false;
   }
 
   // カウントした時間をグラフに反映させる関数
@@ -208,69 +110,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const endTime = new Date().getTime();
     const elapsed = Math.floor((endTime - startTime) / 1000) / 60; // 分単位
     const mode = modeSelect.value;
-    const categoryId = categorySelect.value; // カテゴリ選択のID（カテゴリ選択UIがある場合）
 
-    fetch("/home/api/store_elapsed_time/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        mode: mode,
-        elapsed: elapsed,
-        category_id: categoryId, // 選択されたカテゴリ
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("記録が保存されました:", data);
-        window.dispatchEvent(new Event("updateGraph")); // グラフを更新
-      })
-      .catch((error) => {
-        console.error("時間の保存に失敗しました:", error);
-      });
-  }
+    let data = JSON.parse(localStorage.getItem("timerData")) || {
+      input: 0,
+      output: 0,
+    }; // ローカルストレージに保存されたtimerDataを取得し、JSON.parse() で JavaScript オブジェクトに変換
+    data[mode] += elapsed; // カウントした時間をmode（"input" または "output"）ごとに累積させる
+    localStorage.setItem("timerData", JSON.stringify(data)); //更新されたデータを JSON.stringify() で文字列に変換し、再び localStorage に保存
 
-  // 作業時間を取得する関数
-  function getWorkTime() {
-    fetch("/home/api/get_study_time/")
-      .then((response) => response.json())
-      .then((data) => {
-        let workTime = data.study || 25;
-        workTime = parseInt(workTime, 10);
-        let hours = Math.floor(workTime / 60);
-        let minutes = workTime % 60;
-        countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
-          minutes
-        ).padStart(2, "0")}:00`;
-      })
-      .catch((error) => {
-        console.error("作業時間の取得に失敗してますよ！！", error);
-      });
+    window.dispatchEvent(new Event("updateGraph")); // new Event(eventtype)によって、カスタムイベントである"updateGraph"を定義し、window.dispatchEventメソッドによって、カスタムイベントを発火させ、graph.jsにあるwindow.addEventListenerを動かす
   }
 
   startButton.addEventListener("click", startTimer);
   pauseButton.addEventListener("click", pauseTimer);
   resumeButton.addEventListener("click", resumeTimer);
   resetButton.addEventListener("click", resetTimer);
-  stopRestButton.addEventListener("click", stopRestTimer);
-  getWorkTime();
 
-  // window.addEventListener("load", function () {
-  //   // 作業時間を取得
-  //   fetch("/home/api/get_study_time/")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       let workTime = data.study || 25;
-  //       workTime = parseInt(workTime, 10);
-  //       let hours = Math.floor(workTime / 60);
-  //       let minutes = workTime % 60;
-  //       countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
-  //         minutes
-  //       ).padStart(2, "0")}:00`;
-  //     })
-  //     .catch((error) => {
-  //       console.error("作業時間の取得に失敗してますよ！！", error);
-  //     });
-  // });
+  window.addEventListener("load", function () {
+    let workTime = localStorage.getItem("workTime") || 60;
+    workTime = parseInt(workTime, 10);
+    let hours = Math.floor(workTime / 60);
+    let minutes = workTime % 60;
+    countDown.textContent = `${String(hours).padStart(2, "0")}:${String(
+      minutes
+    ).padStart(2, "0")}:00`;
+  });
 });
--- src/app/urls.py
+++ src/app/urls.py
@@ -5,22 +5,15 @@ from .views import LearningSummary
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('setting/', views.SettingView.as_view(), name='setting'),
-    path('setting/api/add_category/', views.add_category, name='add_category'),
-    path('setting/api/delete_category/', views.delete_category, name='delete_category'),
-    path('setting/api/save_work_time/', views.save_work_time, name='save_work_time'),
-    path('setting/api/save_rest_time/', views.save_rest_time, name='save_rest_time'),
+    # path('home/', views.home, name='home'),
+    path('setting/', views.setting, name='setting'),
+
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('home/', views.HomeView.as_view(), name='home'),
-    path('home/api/get_study_time/', views.get_study_time, name='get_study_time'),
-    path('home/api/get_rest_time/', views.get_rest_time, name='get_rest_time'),
-    path('home/api/store_elapsed_time/', views.store_elapsed_time, name='store_elapsed_time'),
-    path('home/api/get_timer_data/', views.get_timer_data, name='get_timer_data'),
-    path('home/api/get_categories/', views.get_categories, name='get_categories'),
-    path('home/api/update_chart/', views.update_chart, name='update_chart'),
     path('signup/', views.Signup.as_view(), name='signup'),
     path('account/', views.update_profile, name='account'),
-    path('summary', LearningSummary.as_view(), name='summary'),
+    
+        path('summary', LearningSummary.as_view(), name='summary'),
     path('summary/<str:period>', LearningSummary.as_view(), name='summary_api')
 ]   
