9ed6fa4aa059fd7e6fee3e40a0cfa59e8ef0207f
miroku-tanabe-369 <mirojiri36@gmail.com>
2025-02-15 23:10:58 +0900

    add global css, home.html, setting.html etc

--- src/app/views.py
+++ src/app/views.py
@@ -4,3 +4,9 @@ from .models import User, Timer, Category, Study_log, Goal
 def index(request):
     categories = Category.objects.all() 
     return render(request, 'app/index.html' , {'categories': categories})
+
+def home(request):
+    return render(request, 'app/home.html')
+
+def setting(request):
+    return render(request, 'app/setting.html')
